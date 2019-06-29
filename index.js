14ce851ed766e801019b34e6a87d20f131e2b755
johncerpa <jjcerpa@uninorte.edu.co>
2019-06-29 00:11:44 -0500

    Deploying

--- index.js
+++ index.js
@@ -3,27 +3,39 @@ const app = express ();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const cors = require('cors');
+const path = require('path');
+const morgan = require('morgan');
+const compression = require('compression');
+const mongoose = require('mongoose');
+const Message = require('./MessageModel').default;
+
+const dev = app.get('env') !== 'production';
+
+if (!dev) {
+  app.disable('x-powered-by');
+  app.use(compression());
+  app.use(morgan('common'));
+  app.use(express.static(path.join(__dirname, 'build')));
+}
+
+if (dev) {
+  app.use(morgan('dev'));
+}
 
 server.listen(process.env.PORT || 8000);
-console.log('Server is now listening to requests...');
 
 io.on('connection', client => {
-  console.log('A new client connected to the server');
-
   // Receives client's message then sends to all except the sender
   client.on('message', data => {
     client.broadcast.emit('messageReceived', {name: data.name, message: data.message });
   })
-
-})
+});
 
 // Database handling
-const mongoose = require('mongoose');
-const DB_PWD = process.env.DB_PWD|| 'Jh0n3116552508*';
+const DB_PWD = process.env.DB_PWD;
 const URI = `mongodb+srv://admin:${DB_PWD}@cluster0-08xi0.mongodb.net/chat?retryWrites=true&w=majority`;
 mongoose.connect(URI, {useNewUrlParser: true});
 mongoose.set('useCreateIndex', true);
-const Message = require('./MessageModel').default;
 
 app.use(express.json());
 app.use(cors())
